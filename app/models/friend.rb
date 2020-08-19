eb7aa4f45251e5f3e3ec447893aa0c22b37d08fe
Emily Chen <snoopy850321@gmail.com>
2020-08-19 08:05:29 +0800

    CommentOutFriendsModel

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,5 @@
 class Friend < ApplicationRecord
-    validates :user_first_id, :user_second_id, presence: true 
-    validates  :user_first_id, uniqueness: {scope: :user_second_id}
-    validatea :user_second_id, uniqueness: {scope: :user_first_id} 
+    # validates :user_first_id, :user_second_id, presence: true 
+    # validates  :user_first_id, uniqueness: {scope: :user_second_id}
+    # validates :user_second_id, uniqueness: {scope: :user_first_id} 
 end 
